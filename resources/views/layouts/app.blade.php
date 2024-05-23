7a3c06599dbddce143115b86bd93385c589ea699
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-05-23 13:56:16 +0300

    push to host

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ClickMusic Ro</title>
+    <title>Click Music - Muzica, Hip-Hop, Soul, Reggae</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
