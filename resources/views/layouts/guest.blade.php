c5e824e85ecfe65ea0e45fbb9da63355a844c592
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-05-24 12:26:40 +0300

    google tag

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ro">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-34NT57GG5F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-        <title>ClickMusic Ro</title>
+  gtag('config', 'G-34NT57GG5F');
+</script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Meta Tags for Click Music Streaming App -->
+<meta name="description" content="Click Music - Muzica, Hip-Hop, Soul, Reggae - O aplicație de streaming video a artistului de muzică hip-hop, reggae și soul Click din Baia Mare, Maramureș, Romania" />
+<meta name="keywords" content="Click Music, streaming video, hip-hop, reggae, soul, Click, Baia Mare, Maramureș, Romania, muzică, videoclipuri muzicale, artist, streaming, audio, video" />
+
+<!-- Open Graph Tags for Social Media Sharing -->
+<meta property="og:title" content="Click Music - Muzica, Hip-Hop, Soul, Reggae" />
+<meta property="og:description" content="Click Music - Muzica, Hip-Hop, Soul, Reggae - O aplicație de streaming video a artistului de muzică hip-hop, reggae și soul Click din Baia Mare, Maramureș, Romania" />
+<meta property="og:image" content="{{ asset('img/ClickMusic-OG.jpg') }}" /> 
+<meta property="og:image:type" content="image/jpg" />
+<meta property="og:image:alt" content="Click Music - Muzica, Hip-Hop, Soul, Reggae" />
+<meta property="og:url" content="https://clickmusic.ro" />
+<meta property="og:type" content="website" />
+<meta property="og:locale" content="ro_RO" />
+<meta property="og:site_name" content="Click Music - Muzica, Hip-Hop, Soul, Reggae" />
+
+
+<link rel="canonical" href="https://clickmusic.ro" />
+
+<!-- Favicon -->
+<link rel="icon" href="{{ asset('img/logo.png') }}" type="image/x-icon" />
+
+<!-- Apple Touch Icon -->
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/logo.png') }}" type="image/x-icon" />
+
+
+    <title>Click Music - Muzica, Hip-Hop, Soul, Reggae</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
