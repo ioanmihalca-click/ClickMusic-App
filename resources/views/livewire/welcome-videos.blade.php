c4b213547814b05e42006f482503cb6f52402601
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-05-27 15:30:36 +0300

    welcome-videos

--- resources/views/livewire/welcome-videos.blade.php
+++ resources/views/livewire/welcome-videos.blade.php
@@ -4,12 +4,12 @@
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
         @foreach ($videos as $video)
             <div class="max-w-xs mx-0 overflow-hidden rounded-lg shadow-lg sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
-                <a href="{{ route('videos.show', $video) }}">
+              
                     <div class="mb-4 aspect-w-16 aspect-h-9">
                         <img src="{{ $video->thumbnail_url }}" alt="{{ $video->title }}">
                     </div>
-                    <h3 class="pl-1 mb-1 text-base font-semibold">{{ $video->title }}</h3>
-                </a>
+                    <h3 class="pl-1 mb-1 text-sm font-normal">{{ $video->title }}</h3>
+                
             </div>
         @endforeach
     </div>
