afb1cf7e8f97ca7cdcf4e297b9a6a6220f5ebc86
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-05-27 18:23:56 +0300

    thumbnail

--- resources/views/videos/share.blade.php
+++ resources/views/videos/share.blade.php
@@ -8,9 +8,12 @@
     <title>{{ $video->title }}</title>
     <meta property="og:title" content="{{ $video->title }}" />
     <meta property="og:description" content="{{ $video->description }}" />
-    <meta property="og:image" content="{{ asset('img/ClickMusic-OG.jpg') }}" />
+    <meta property="og:image" content="{{ $video->thumbnail_url }}" />
+    <meta property="og:image:width" content="1920" />
+    <meta property="og:image:height" content="1080" />
+
     <meta property="og:image:type" content="image/jpg" />
-    <meta property="og:image:alt" content="Click Music - Muzica, Hip-Hop, Soul, Reggae" />
+    <meta property="og:image:alt" content="{{ $video->title }}" />
 
     <meta property="og:url" content="{{ route('videos.share', $video->id) }}" />
     <meta property="og:type" content="video.other" />
