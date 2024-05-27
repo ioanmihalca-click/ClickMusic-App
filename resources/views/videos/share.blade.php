41b04af746ae6e093e3ffe567c1059ecb96668f8
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-05-27 17:58:18 +0300

    thumbnail

--- resources/views/videos/share.blade.php
+++ resources/views/videos/share.blade.php
@@ -9,8 +9,8 @@
     <meta property="og:title" content="{{ $video->title }}" />
     <meta property="og:description" content="{{ $video->description }}" />
     <meta property="og:image" content="{{ $video->thumbnail_url }}" />
-    <meta property="og:image:width" content="1200" />
-<meta property="og:image:height" content="630" />
+    <meta property="og:image:width" content="1920" />
+    <meta property="og:image:height" content="1080" />
     <meta property="og:url" content="{{ route('videos.share', $video->id) }}" />
     <meta property="og:type" content="video.other" />
 
@@ -19,14 +19,18 @@
 </head>
 
 <body>
-    <div class="max-w-md mx-auto mt-8 text-left rounded-lg bg-white p-4 shadow-lg ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-blue-500/20 focus:outline-none focus-visible:ring-[#FF2D20]">
+    <div
+        class="max-w-md mx-auto mt-8 text-left rounded-lg bg-white p-4 shadow-lg ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-blue-500/20 focus:outline-none focus-visible:ring-[#FF2D20]">
         <div class="mb-4 aspect-w-16 aspect-h-9">
-            <img src="{{ $video->thumbnail_url }}" alt="{{ $video->title }}" class="object-cover w-full h-full rounded-md">
+            <img src="{{ $video->thumbnail_url }}" alt="{{ $video->title }}"
+                class="object-cover w-full h-full rounded-md">
         </div>
 
         <h1 class="mb-2 text-xl font-semibold">{{ $video->title }}</h1>
         <p class="text-gray-700">{{ $video->description }}</p>
-        <p class="mt-4 text-center text-gray-500">Pentru a urmari videoul complet va rugam sa va <a href='/register' class="px-2 text-white bg-blue-500 rounded-sm">inregistrati</a> sau sa va <a href='/login' class="px-2 text-white bg-blue-500 rounded-sm">logati</a>.</p>
+        <p class="mt-4 text-center text-gray-500">Pentru a urmari videoul complet va rugam sa va <a href='/register'
+                class="px-2 text-white bg-blue-500 rounded-sm">inregistrati</a> sau sa va <a href='/login'
+                class="px-2 text-white bg-blue-500 rounded-sm">logati</a>.</p>
     </div>
 </body>
 
