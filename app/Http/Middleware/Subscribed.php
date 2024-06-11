6add8eb2575d5cecafaf5b735faebadf1cc04df6
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-06-11 13:11:00 +0300

    abonament-stripe

---
+++ app/Http/Middleware/Subscribed.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Subscribed
+{
+
+     
+    public function handle(Request $request, Closure $next): Response
+    {
+        // if (! $request->user()?->subscribed()) {
+        //     // Redirect user to billing page and ask them to subscribe...
+        //     return redirect('abonament');
+        // }
+
+        return $next($request);
+    }
+}
--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,5 +1,6 @@
 <nav class="flex justify-end flex-1 mt-4 -mx-3">
     @auth
+
         <a
             href="{{ url('/videoclipuri') }}"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] "
