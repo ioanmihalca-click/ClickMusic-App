2dc7c7e964fe0780e3212f834bb54d3ddf60e9b4
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-06-11 22:15:03 +0300

    subscription

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -4,25 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
+use App\Http\Controllers\Controller;
 
-class StripeWebhookController extends CashierController
+class StripeWebhookController extends Controller
 {
-    /**
-     * Handle the incoming webhook.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function handleWebhook(Request $request)
     {
         $payload = $request->all();
         $eventType = $payload['type'];
-
+    
         if ($eventType == 'customer.subscription.updated' || $eventType == 'customer.subscription.created') {
             $subscription = $payload['data']['object'];
             $user = User::where('stripe_id', $subscription['customer'])->first();
-
+    
             if ($user) {
                 // Update the user's subscription status in the database
                 $user->subscriptions()
@@ -30,7 +24,8 @@ class StripeWebhookController extends CashierController
                      ->update(['stripe_status' => $subscription['status']]);
             }
         }
-
+    
         return response('Webhook Handled', 200);
     }
+    
 }
