040177e40ca1c011fc1ebeb69504d3c387063b37
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-06-11 14:58:59 +0300

    ab

--- resources/views/abonament.blade.php
+++ resources/views/abonament.blade.php
@@ -75,7 +75,7 @@
   
  
             <header class="flex flex-col items-center justify-center mt-2 mb-4">
-    <a href="{{ route('welcome') }}"> <!-- Adaugă acest element de ancoră -->
+    <a href="{{ route('/') }}"> <!-- Adaugă acest element de ancoră -->
         <img src="/img/logo.png" alt="Logo Click Music"
             class="h-12 w-auto text-white lg:h-16 lg:text-[#FF2D20] mt-8">
     </a>
