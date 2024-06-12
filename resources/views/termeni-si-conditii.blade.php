fd6cb24047298a26a60efc2aa41bfcfc2c9e2f5e
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-06-12 16:54:00 +0300

    subcription welcome

--- resources/views/termeni-si-conditii.blade.php
+++ resources/views/termeni-si-conditii.blade.php
@@ -15,14 +15,16 @@
             <p class="mb-4">Prin accesarea și utilizarea aplicației noastre, sunteți de acord să respectați acești termeni și condiții și toate legile și reglementările aplicabile.</p>
             <h2 class="mb-2 text-2xl font-semibold">2. Contul Utilizatorului</h2>
             <p class="mb-4">Pentru a accesa anumite funcționalități ale aplicației, este posibil să fie necesar să vă înregistrați un cont de utilizator. Sunteți responsabil pentru păstrarea confidențialității informațiilor de autentificare ale contului dvs.</p>
-            <h2 class="mb-2 text-2xl font-semibold">3. Drepturile de Autor</h2>
+            <h2 class="mb-2 text-2xl font-semibold">3. Abonament</h2>
+            <p class="mb-4">Pentru acces complet la întreaga colecție de videoclipuri și vloguri, inclusiv premiere în exclusivitate și cele mai noi lansări, este disponibil un abonament la costul de 9,99 lei/lună, sau 99.99 lei/an. Abonamentul poate fi anulat oricând.</p>
+            <h2 class="mb-2 text-2xl font-semibold">4. Drepturile de Autor</h2>
             <p class="mb-4">Toate drepturile de autor și alte drepturi de proprietate intelectuală asupra conținutului și materialelor disponibile în aplicație sunt deținute de noi sau de licențiatorii noștri.</p>
-            <h2 class="mb-2 text-2xl font-semibold">4. Limitări</h2>
+            <h2 class="mb-2 text-2xl font-semibold">5. Limitări</h2>
             <p class="mb-4">În niciun caz nu vom fi responsabili pentru orice daune directe, indirecte, speciale, conexe sau punitive sau orice daune, inclusiv, fără limitare, pierderile de profit, venituri, economii sau date rezultate din utilizarea sau imposibilitatea utilizării aplicației noastre.</p>
-            <h2 class="mb-2 text-2xl font-semibold">5. Modificări ale Termenilor și Condițiilor</h2>
+            <h2 class="mb-2 text-2xl font-semibold">6. Modificări ale Termenilor și Condițiilor</h2>
             <p class="mb-4">Ne rezervăm dreptul de a actualiza sau modifica periodic acești termeni și condiții fără notificare prealabilă. Vă recomandăm să verificați periodic această pagină pentru a fi la curent cu orice modificări.</p>
-            <h2 class="mb-2 text-2xl font-semibold">6. Contact</h2>
-            <p class="mb-4">Dacă aveți întrebări sau nelămuriri cu privire la acești termeni și condiții, vă rugăm să ne contactați la adresa <a href="mailto:contact@clickmusic.com" class="text-blue-500">clickmusicro@gmail.com</a>.</p>
+            <h2 class="mb-2 text-2xl font-semibold">7. Contact</h2>
+            <p class="mb-4">Dacă aveți întrebări, nelămuriri sau probleme legate de acești termeni și condiții sau de abonament, vă rugăm să ne contactați la adresa <a href="mailto:clickmusicro@gmail.com" class="text-blue-500">clickmusicro@gmail.com</a>.</p>
         </div>
     </div>
 </body>
