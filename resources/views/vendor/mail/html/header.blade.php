f3e5767935440e67bdb100a2712f406e1897be0e
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-06-14 16:01:21 +0300

    notificari email abonamente

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <tr>
 <td class="header">
 <a href="https://clickmusic.ro/" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://clickmusic.ro/img/logo.png" class="logo" alt="">
+@if (trim($slot) === 'Click Music')
+<img src="" class="logo" alt="">
 @else
 {{ $slot }}
 @endif
