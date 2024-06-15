557d1c5196737a1ad4dd804de19175cb7571de7b
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-06-15 23:20:23 +0300

    queue

--- app/Notifications/SubscriptionCreated.php
+++ app/Notifications/SubscriptionCreated.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SubscriptionCreated extends Notification
+class SubscriptionCreated extends Notification implements ShouldQueue
 {
     use Queueable;
 
