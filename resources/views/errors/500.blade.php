d4e585cee21aaf7fd3e04328099848102df765b9
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-06-19 14:32:13 +0300

    notificare mail video nou

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 Not Found</title>
+    <title>500 Server Error</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
     <div class="flex items-center justify-center min-h-screen">
-        <div class="max-w-md p-8 mx-auto bg-white rounded-lg shadow-md">
-            <div class="mb-6 text-center">
-                <h1 class="mb-4 text-4xl font-bold text-gray-800">500</h1>
-                <p class="text-gray-600">Pagina pe care o cautati nu exista, sau nu sunteti autentificat in aplicatia Click Music.</p>
-            </div>
+     
+         <div class="px-2 text-center bg-gray-100">
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="font-extrabold text-red-500 text-8xl">500</h1>
+        <p class="text-4xl font-medium text-gray-800">Internal Server Error</p>
+        <p class="mt-4 text-xl text-gray-800">Pagina pe care o cautati nu exista, sau nu sunteti autentificat in aplicatia Click Music.</p>
+    </div>
+</div>
        
             <div class="text-center">
                 <a href="{{ url('/') }}" rel="noopener noreferrer" class="inline-block px-4 py-2 font-semibold text-white bg-indigo-500 rounded hover:bg-indigo-600">
