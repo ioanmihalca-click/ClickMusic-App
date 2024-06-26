b899c5f6f624aa2df83b950beacaa3c4432a0256
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-06-26 17:33:09 +0300

    evo

--- app/Http/Middleware/Subscribed.php
+++ app/Http/Middleware/Subscribed.php
@@ -9,14 +9,16 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Subscribed
 {
-
-     
     public function handle($request, Closure $next)
     {
-        if ($request->user() && ($request->user()->subscribed('prod_QGao8eve2XHvzf') || $request->user()->usertype === 'super_user')) {
+        if ($request->user() && (
+            $request->user()->subscribed('prod_QGao8eve2XHvzf') || 
+            $request->user()->usertype === 'super_user' ||
+            $request->user()->usertype === 'admin' // Allow admins as well
+        )) {
             return $next($request);
         }
 
-        return redirect('abonament'); // Or any other relevant page
+        return redirect('abonament'); 
     }
 }
