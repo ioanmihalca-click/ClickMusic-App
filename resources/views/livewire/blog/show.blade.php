161cd16116fa94652e6a3203bcbb7183d400c247
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-07-06 19:07:13 +0300

    evo blog

--- resources/views/livewire/blog/show.blade.php
+++ resources/views/livewire/blog/show.blade.php
@@ -1,6 +1,6 @@
 
-<article class="max-w-4xl p-8 mx-auto bg-white rounded-lg shadow-md">
-    <h1 class="mb-4 text-4xl font-bold text-gray-800">{{ $post->title }}</h1>
+<article class="max-w-4xl p-4 mx-auto bg-white rounded-lg shadow-md">
+    <h1 class="mb-4 text-2xl font-bold text-gray-800 md:text-4xl">{{ $post->title }}</h1>
 
     <div class="mb-4 text-gray-600">
         Publicat la data de {{ $post->published_at->format('F j, Y') }} de Click
