49e9d9b846f3fcd229e7404ea7ee9537dc87241c
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-12-11 18:31:47 +0200

    evo

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -9,14 +9,15 @@ use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
 use Filament\Tables\Actions\Action;
+use Illuminate\Support\Facades\Auth;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
+use App\Megaphone\SuperUserNotificationBell;
 use App\Notifications\SuperUserNotification;
 use App\Filament\Resources\UserResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\UserResource\RelationManagers;
-use App\Megaphone\SuperUserNotificationBell;
 
 class UserResource extends Resource
 {
@@ -31,7 +32,7 @@ class UserResource extends Resource
             ->schema([
                 TextInput::make('name')->required(),
                 TextInput::make('email')->email()->required(),
-                TextInput::make('password')->password()->required()->visibleOn('create'),
+                TextInput::make('password')->password()->required(),
                 Select::make('usertype')->options([
                     'admin' => 'admin',
                     'user' => 'user',
@@ -58,7 +59,7 @@ class UserResource extends Resource
                 Action::make('make_super_user')
                 ->label('Make Super User')
                 ->action(function (User $record) {
-                    if ($record->id == auth()->id()) { 
+                    if ($record->id == Auth::id()) { 
                         return redirect()->back()->with('error_message', 'You cannot change your own user type.');
                     }
                     $record->update(['usertype' => 'super_user']);
