2add33d2495e179b3b68dee159e5483633139fd2
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-12-28 17:29:27 +0200

    dashboard evo

--- resources/views/videoclipuri.blade.php
+++ resources/views/videoclipuri.blade.php
@@ -13,10 +13,10 @@
             <div class="w-24 h-1 mx-auto mt-2 rounded-full bg-gradient-to-r from-blue-500 to-transparent"></div>
             
             <p class="max-w-2xl mx-auto mt-6 text-center text-gray-300">
-                Te salut și îți mulțumesc că ai ajuns pe pagina asta. Aceasta comunitate este un club exclusiv. <a href="{{ route('forum.index') }}" class="text-blue-400 transition-colors hover:text-blue-300">Te invit să participi la discuțiile din forumul "Comunitate"</a>.
+                Te salut și îți mulțumesc că ai ajuns pe pagina asta. Aceasta comunitate este un club exclusiv. Te invit să participi la discuțiile din <a href="{{ route('forum.index') }}" class="text-blue-400 transition-colors hover:text-blue-300"> forumul "Comunitate"</a>.
             </p>
             <p class="max-w-2xl mx-auto mt-6 text-center text-gray-300">
-              Aici vei gasi mereu toate piesele mele inainte de a fi lansate. 
+            
               Am pus recent doua piese noi, nelansate inca: "RESPECT" si "Bate inima ca bitu"
             </p>
             
