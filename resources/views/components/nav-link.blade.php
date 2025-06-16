677134ed068b85c9ac4e9539a6a3733332c64798
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2025-06-16 10:38:43 +0300

    forum

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-    ? 'inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-blue-400 focus:outline-none focus:border-blue-600 transition duration-300 ease-in-out'
-    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-400 hover:text-blue-400 hover:border-blue-500/50 focus:outline-none focus:text-blue-400 focus:border-blue-500/50 transition duration-300 ease-in-out';
+    ? 'inline-flex items-center px-2 pt-1 border-b-2 border-blue-400 text-sm font-medium leading-5 text-blue-400 focus:outline-none focus:border-blue-600 transition duration-300 ease-in-out'
+    : 'inline-flex items-center px-2 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-300 hover:text-blue-400 hover:border-blue-400/50 focus:outline-none focus:text-blue-400 focus:border-blue-400/50 transition duration-300 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-indigo-400 text-start text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-blue-400 text-start text-base font-medium text-blue-400 focus:outline-none focus:text-blue-300 focus:border-blue-500 transition-all duration-200 ease-in-out'
+            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-300 hover:text-blue-400 hover:border-blue-400/50 focus:outline-none focus:text-blue-400 focus:border-blue-400 transition-all duration-200 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
