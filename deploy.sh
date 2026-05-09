5add364c94afbef2e5768b406fdd8b2d4bbb5f74
Click Studios Digital <ioanclickmihalca@gmail.com>
2026-05-09 11:12:13 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ git clean -fd \
 
 git pull origin main
 
-/opt/alt/php83/usr/bin/php composer.phar install --no-dev --optimize-autoloader
-/opt/alt/php83/usr/bin/php artisan migrate --force
-/opt/alt/php83/usr/bin/php artisan config:cache
-/opt/alt/php83/usr/bin/php artisan route:cache
-/opt/alt/php83/usr/bin/php artisan view:cache
+/opt/alt/php84/usr/bin/php composer.phar install --no-dev --optimize-autoloader
+/opt/alt/php84/usr/bin/php artisan migrate --force
+/opt/alt/php84/usr/bin/php artisan config:cache
+/opt/alt/php84/usr/bin/php artisan route:cache
+/opt/alt/php84/usr/bin/php artisan view:cache
